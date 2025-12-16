1d311b0d4432bf3cc2dc29d346f71146bef225cd
Daniil Peretjaka <daniil.peretjaka@ivkhk.ee>
2025-12-16 13:25:27 +0200

    chore: make main.ts initialization async

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,15 +1,17 @@
-import { AppView } from './views/AppView.js';
-import { AppController } from './controllers/AppController.js';
+import { AppView } from './views/AppView';
+import { AppController } from './controllers/AppController';
 
 // Get root container where the application will be rendered
 const root = document.getElementById('app');
 
 // Initialize the application only if the root element exists
 if (root) {
+  (async () => {
   // Create the main view and controller
   const view = new AppView(root);
   const controller = new AppController(view);
 
   // Start the application (sets up event listeners, renders UI, etc.)
   controller.init();
+  })();
 }
